b7fcb28f80cb1b7dabba352269cecd8ad926ba51
David Mwangi <david.mathenge98@gmail.com>
2018-11-12 18:49:55 +0300

    maintain(end-to-end tests): add e2e test for paragraph renders correctly

--- react-puppeteer/src/App.js
+++ react-puppeteer/src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p className="Paragraph">
+            Welcome to React
+          </p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
--- react-puppeteer/src/App.test.js
+++ react-puppeteer/src/App.test.js
@@ -2,8 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const faker = require('faker');
+const puppeteer = require('puppeteer');
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Paragraph', () => {
+  it('renders paragraph correctly', async () => {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+  
+    page.emulate({
+      viewport: {
+      width: 500,
+      height: 2400
+      },
+      userAgent: ''
+    });
+
+    await page.goto('http://localhost:3001/');
+	  await page.waitForSelector('.Paragraph');
+
+	  const html = await page.$eval('.Paragraph', e => e.innerHTML);
+	  expect(html).toBe('Welcome to React');
+
+	  browser.close();
+  }, 16000);
+});
+
