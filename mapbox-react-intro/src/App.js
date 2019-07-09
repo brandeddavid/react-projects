2780d71bf18b4eed5865048a5b9087996fd7acd0
David Mwangi <david.mathenge98@gmail.com>
2019-07-09 12:12:42 +0300

    feat(render-map): render Nairobi on mapbox

--- mapbox-react-intro/src/App.js
+++ mapbox-react-intro/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl'
 
 function App() {
+  const [ viewport, setViewport] = useState({
+    latitude: -1.28333,
+    longitude: 36.81667,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10,
+
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMapGL
+        { ...viewport }
+        mapboxApiAccessToken = { process.env.REACT_APP_MAPBOX_API_KEY}
+        onViewportChange = { viewport => setViewport(viewport) }
+      >
+
+      </ReactMapGL>
     </div>
   );
 }
